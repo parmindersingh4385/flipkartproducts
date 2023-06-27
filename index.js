0e820fc0dc86cfa64d73c3efe69eb1f9453c162e
Parminder Singh <parmindersingh@sdplabs.com>
2023-06-27 16:31:40 +0530

    Commit 3

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.get('/:id', async function(req, res){
     
 });
 
-app.listen(port, function(req, res){
-    console.log('App listening on port:--- ' + port);
+app.listen(port, '0.0.0.0', function () {
+	console.log('App running on port ' + port);
 });
