c8b6155acf78f5adba93d7b0df1798a3301c9158
Parminder singh <singh1.parminder@gmail.com>
2023-06-27 23:14:53 +0530

    Commit 1

--- index.js
+++ index.js
@@ -30,95 +30,99 @@ const PRODUCTS = mongoose.model('tbl_products', {
 });
 
 // Load the session data
-mongoose.connect(
-	'mongodb+srv://parminder:9988641591%40ptk@cluster0-ix992.mongodb.net/db_products?retryWrites=true&w=majority',
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	}
-)
-.then(() => {
-    scheduleJobForGf();
-});
+mongoose
+	.connect(
+		'mongodb+srv://parminder:9988641591%40ptk@cluster0-ix992.mongodb.net/db_products?retryWrites=true&w=majority',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		}
+	)
+	.then(() => {
+		scheduleJobForGf();
+	});
 
-function scheduleJobForGf(){
-    schedule.scheduleJob('*/1 * * * *', function () {
-        const groupName = 'GirlsFab';
-        sendToTelegramChannel(groupName);
-    });
+function scheduleJobForGf() {
+	schedule.scheduleJob('*/1 * * * *', function () {
+		const groupName = 'GirlsFab';
+		sendToTelegramChannel(groupName);
+	});
 }
 
 async function sendToTelegramChannel(groupName) {
-    try {
-        let randomProduct = await PRODUCTS.find({
-            source: groupName.toLowerCase()
-        }).limit(1);
+	try {
+		let randomProduct = await PRODUCTS.find({
+			source: groupName.toLowerCase()
+		}).limit(1);
 
-        if (randomProduct && randomProduct.length > 0) {
-            let retData = randomProduct[0];
+		if (randomProduct && randomProduct.length > 0) {
+			let retData = randomProduct[0];
 
-            var api = new telegram({
-                token: '6158204123:AAGoADPhxzS8wQGO8DeLWwZr6g8gpoQbSLo',
-                async_requests: true,
-                updates: {
-                    enabled: true,
-                    get_interval: 1000
-                }
-            });
+			var api = new telegram({
+				token: '6158204123:AAGoADPhxzS8wQGO8DeLWwZr6g8gpoQbSLo',
+				async_requests: true,
+				updates: {
+					enabled: true,
+					get_interval: 1000
+				}
+			});
 
-            api.sendPhoto({
-                chat_id: '@' + groupName, //'@GirlsFab',
-                caption: `${retData.title} ${retData.purchase_url}`,
-                photo: retData.image_url[retData.image_url.length - 1]
-            }).then(function (data) {
-                deleteAfterSent(retData.product_id);
-            });
-        }
-    }
-    catch (err) {}
+			api.sendPhoto({
+				chat_id: '@' + groupName, //'@GirlsFab',
+				caption: `${retData.title} ${retData.purchase_url}`,
+				photo: retData.image_url[retData.image_url.length - 1]
+			}).then(function (data) {
+				deleteAfterSent(retData.product_id);
+			});
+		}
+	} catch (err) {}
 }
 
 async function deleteAfterSent(productId) {
-    const result = await PRODUCTS.findOneAndDelete({
-        product_id: productId
-    });
-    if (!result) {
-         
-    } else {
-         
-    }
+	const result = await PRODUCTS.findOneAndDelete({
+		product_id: productId
+	});
+	if (!result) {
+	} else {
+	}
 }
 
-app.get('/', function(req, res){
-    res.json({
-        success: true,
-        message: 'App working successfully................'
-    });
+app.get('/', function (req, res) {
+	res.json({
+		success: true,
+		message: 'App working successfully................123'
+	});
 });
 
-app.post('/:source/:id', async function(req, res){
-    const productId = req.params.id,
-        source = req.params.source;
+app.post('/:source/:id', async function (req, res) {
+	const productId = req.params.id,
+		source = req.params.source;
 
-    try {
-        const result = await PRODUCTS.find({ product_id: productId });
-        if (result.length > 0) {
-            res.json({
-                success: true,
-                message: 'Product already exists'
-            });
-        } else {
-            const response = await axios.get('https://affiliate-api.flipkart.net/affiliate/1.0/product.json?id=' + productId, {
-                headers:{
-                    'Fk-Affiliate-Id': 'singh1par',
-                    'Fk-Affiliate-Token': '1d5f2616c8c644f2806fe8da0c40946e'
-                }
-            }); 
-            const dataObj = response.data.productBaseInfoV1;
+	try {
+		const result = await PRODUCTS.find({ product_id: productId });
+		if (result.length > 0) {
+			res.json({
+				success: true,
+				message: 'Product already exists'
+			});
+		} else {
+			const response = await axios.get(
+				'https://affiliate-api.flipkart.net/affiliate/1.0/product.json?id=' +
+					productId,
+				{
+					headers: {
+						'Fk-Affiliate-Id': 'singh1par',
+						'Fk-Affiliate-Token': '1d5f2616c8c644f2806fe8da0c40946e'
+					}
+				}
+			);
+			const dataObj = response.data.productBaseInfoV1;
 
-            var imagesArray =  Object.entries(dataObj.imageUrls).map((e) => ( e[1] ));
+			var imagesArray = Object.entries(dataObj.imageUrls).map(
+				(e) => e[1]
+			);
 
-            var newProduct = new PRODUCTS({
+			var newProduct = new PRODUCTS({
 				title: dataObj.title,
 				product_id: productId,
 				description: dataObj.productDescription,
@@ -126,12 +130,14 @@ app.post('/:source/:id', async function(req, res){
 				image_url: imagesArray,
 				//brand_url: dataObj.brand_url,
 				purchase_url: dataObj.productUrl,
-				price: dataObj?.flipkartSpecialPrice?.amount || dataObj?.flipkartSellingPrice?.amount,
+				price:
+					dataObj?.flipkartSpecialPrice?.amount ||
+					dataObj?.flipkartSellingPrice?.amount,
 				source: source,
 				is_active: dataObj.inStock
 			});
 
-            const result = await PRODUCTS.find({ product_id: productId });
+			const result = await PRODUCTS.find({ product_id: productId });
 			if (result.length > 0) {
 				res.json({
 					success: true,
@@ -144,37 +150,37 @@ app.post('/:source/:id', async function(req, res){
 					data: retData
 				});
 			}
+		}
+	} catch (err) {
+		res.json({
+			success: false,
+			message: err.message
+		});
+	}
 
-        }
-    }
-    catch (err) { 
-        res.json({
-            success: false,
-            message: err.message
-        });
-    }
+	try {
+		const response = await axios.get(
+			'https://affiliate-api.flipkart.net/affiliate/1.0/product.json?id=' +
+				productId,
+			{
+				headers: {
+					'Fk-Affiliate-Id': 'singh1par',
+					'Fk-Affiliate-Token': '1d5f2616c8c644f2806fe8da0c40946e'
+				}
+			}
+		);
+		const dataObj = response.data.productBaseInfoV1;
 
-    try{
-        const response = await axios.get('https://affiliate-api.flipkart.net/affiliate/1.0/product.json?id=' + productId, {
-            headers:{
-                'Fk-Affiliate-Id': 'singh1par',
-                'Fk-Affiliate-Token': '1d5f2616c8c644f2806fe8da0c40946e'
-            }
-        }); 
-        const dataObj = response.data.productBaseInfoV1;
-
-        res.json({
-            success: true,
-            data: dataObj
-        });
-    }
-    catch (err) { 
-        res.json({
-            success: false,
-            message: err.message
-        });
-    }
-    
+		res.json({
+			success: true,
+			data: dataObj
+		});
+	} catch (err) {
+		res.json({
+			success: false,
+			message: err.message
+		});
+	}
 });
 
 app.listen(port, '0.0.0.0', function () {
