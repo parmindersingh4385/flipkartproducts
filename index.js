24a269465edf41932e61700fed66657222a2cde4
Parminder singh <singh1.parminder@gmail.com>
2023-06-29 00:34:42 +0530

    no message

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ async function deleteAfterSent(productId) {
 app.get('/', function (req, res) {
 	res.status(200).json({
 		success: true,
-		message: 'App working successfully................'
+		message: 'App working successfully................ABC'
 	});
 });
 
